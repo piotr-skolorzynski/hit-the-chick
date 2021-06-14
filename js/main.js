e298b6862e50e6d79065685cbc6419459543bc50
Piotr <p.skolorzynski@gmail.com>
2021-06-14 22:21:03 +0200

    add spaceship add images of chickens modify index.html style.css main.js add trials to move ship

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const spaceShip = document.querySelector('.space-ship');
+
+console.log(spaceShip);
+
+const controlSpaceship = e => {
+    console.log(`${e.code}`);
+
+    if (e.code === 'ArrowRight') {
+        spaceShip.style.left = 75 + '%';
+    } else if (e.code === 'ArrowLeft') {
+        spaceShip.style.left = 25 + '%';
+    }
+
+};
+
+document.addEventListener('keyup', controlSpaceship);
+
+
+    /* const operations = {
+        '+': (a, b) => a + b,
+        '-': (a, b) => a - b,
+        '/': (a, b) => b === 0 ? 'Nie można dzielić przez zero' : a / b,
+        '*': (a, b) => a * b,
+    }
+    return (operations[operator] || (() => 'Nie znany operator'))(num1, num2); */
