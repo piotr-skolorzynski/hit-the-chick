bac695aa9729a5246f2e8f0436119c9711738b0b
Piotr <p.skolorzynski@gmail.com>
2021-08-16 20:13:14 +0200

    modify Enemy2.js file create method animateEnemyMove

--- js/Enemy2.js
+++ js/Enemy2.js
@@ -13,6 +13,7 @@ export class Enemy {
         this.interval = null; //interwał do kontroli animacji przeciwnika
         this.eggIntervalLength = generateNumber(5000, 12000); //losowo ustaw interwał w generowaniu jaj
         this.eggGeneratorInterval = null; //interwał do kontroli generacji jaj
+        this.animateEnemyMoveInterval = null;
     }
 
     init = () => {
@@ -20,6 +21,7 @@ export class Enemy {
         this.animateEnemyAfterHit();
         this.interval = setInterval(() => this.animateEnemyAfterHit(), 1);
         this.eggGeneratorInterval = setInterval(() => this.fireEggs(), this.eggIntervalLength);
+        this.animateEnemyMoveInterval = setInterval(() => this.animateEnemyMove(), 20000);
     }
 
     setEnemyPosition = () => {
@@ -28,6 +30,68 @@ export class Enemy {
         this.gameContainer.appendChild(this.enemyContainer); //umieść w rodzicu
         this.enemyContainer.style.left = `${this.left}%`;
         this.enemyContainer.style.top = `${this.top}%`;
+    }
+    // animuj ruch przeciwników
+    animateEnemyMove = () => {
+        let enemyOnGameboard = document.querySelector(`[data-id="${this.id}"]`);
+        
+        //ustaw interwał na ruch w prawo 
+        const intervalIdNumOne = setInterval(() => {
+            enemyOnGameboard.style.left = `${enemyOnGameboard.offsetLeft + 1}px`;
+        }, 7);
+        
+        //zatrzymaj interwał na ruch w prawo i odpal na ruch w dół
+        let intervalIdNumTwo = null;
+        const timeoutIdNumOne = setTimeout(() => {
+            clearInterval(intervalIdNumOne);
+            enemyOnGameboard = document.querySelector(`[data-id="${this.id}"]`);
+            //ruch w dół
+            intervalIdNumTwo = setInterval(() => {
+                enemyOnGameboard.style.top = `${enemyOnGameboard.offsetTop + 1}px`;
+            }, 10);
+        }, 1200);
+
+        //zatrzymaj interwał na ruch w dół i odpal na ruch w lewo
+        let intervalIdNumThree = null;
+        const timeoutIdNumTwo = setTimeout(() => {
+            clearInterval(intervalIdNumTwo);
+            clearTimeout(timeoutIdNumOne);
+            enemyOnGameboard = document.querySelector(`[data-id="${this.id}"]`);
+            //ruch w lewo
+            intervalIdNumThree = setInterval(() => {
+                enemyOnGameboard.style.left = `${enemyOnGameboard.offsetLeft - 1}px`
+            }, 7);
+        }, 2400);
+
+        //zatrzymaj interwał na ruch w lewo i odpal w górę
+        let intervalIdNumFour = null;
+        const timeoutIdNumThree = setTimeout(() => {
+            clearInterval(intervalIdNumThree);
+            clearTimeout(timeoutIdNumTwo);
+            enemyOnGameboard = document.querySelector(`[data-id="${this.id}"]`);
+            //ruch w górę
+            intervalIdNumFour = setInterval(() => {
+                enemyOnGameboard.style.top = `${enemyOnGameboard.offsetTop - 1}px`;
+            }, 10);
+        }, 4800);
+
+        //zatrzymaj interwał na ruch w górę i odpal w prawo
+        let intervalIdNumFive = null;
+        const timeoutIdNumFour = setTimeout(() => {
+            clearInterval(intervalIdNumFour);
+            clearTimeout(timeoutIdNumThree);
+            enemyOnGameboard = document.querySelector(`[data-id="${this.id}"]`);
+            //ruch w prawo
+            intervalIdNumFive = setInterval(() => {
+                enemyOnGameboard.style.left = `${enemyOnGameboard.offsetLeft + 1}px`;
+            }, 7)
+        }, 6000);
+
+        //zatrzymaj ruch w prawo
+        setTimeout(() => {
+            clearInterval(intervalIdNumFive);
+            clearTimeout(timeoutIdNumFour);
+        }, 7200);
     }
 
     animateEnemyAfterHit = () => {
@@ -39,6 +103,7 @@ export class Enemy {
             }, 1000);
             clearInterval(this.eggGeneratorInterval);
             clearInterval(this.interval);
+            clearInterval(this.animateEnemyMoveInterval);
         }
     }
 
